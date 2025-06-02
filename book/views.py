b1f189c1689d5b307c3e676142c7f183a3255003
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-06-02 19:07:44 -0300

    create book views

--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import Book
 
-# Create your views here.
+
+
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objetcs.all()
+    serializer_class = None
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = None
