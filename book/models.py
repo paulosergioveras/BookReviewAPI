0522c35588edbaa7119dcc67fa67cff8f485319b
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-06-02 18:59:00 -0300

    style: formatação avg_rating

--- book/models.py
+++ book/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
@@ -10,12 +11,15 @@ class Book(models.Model):
     genre = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13, unique=True)
     avg_rating = models.DecimalField(
-        max_digits=3, decimal_places=1, default=0.0,
+        max_digits=2,
+        decimal_places=1,
+        default=0.0,
         validators=[
-            MinValueValidator(0.0), MaxValueValidator(5.0)
-        ])
+            MinValueValidator(0.0),
+            MaxValueValidator(5.0)
+        ]
+    )
     release_date = models.DateField()
     pages = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1)])
     publisher = models.CharField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
