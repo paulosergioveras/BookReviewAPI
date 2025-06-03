59db93cbb5e10c121df56be3ecc692e282b2cf91
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-06-03 20:35:02 -0300

    feat: create views to list, create, retrieve, update and destroy

--- review/views.py
+++ review/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import Review, ReviewSerializer
 
-# Create your views here.
+
+
+
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objetcs.all()
+    serializer_class = ReviewSerializer
+
+class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
