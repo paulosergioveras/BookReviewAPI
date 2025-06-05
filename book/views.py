be6c2757e89d697568c7992c6a8bf0baeb541463
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-06-04 21:17:31 -0300

    feat: add permissions

--- book/views.py
+++ book/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,9 +8,11 @@ from .serializers import BookSerializer
 
 
 class BookListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- review/views.py
+++ review/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from .models import Review
 from .serializers import ReviewSerializer
 
@@ -6,9 +8,11 @@ from .serializers import ReviewSerializer
 
 
 class ReviewListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
